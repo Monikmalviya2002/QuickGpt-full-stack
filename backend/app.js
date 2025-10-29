0509db03dbe6d1e276c41f9be97d51c25a1e30e5
Monik Malviya <monikmalviya04gmail.com>
2025-10-29 12:54:24 +0530

    update the url

--- backend/app.js
+++ backend/app.js
@@ -11,8 +11,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://quick-gpt-full-stack-k27j.vercel.app/",
-    credentials: true,
+    origin: "https://quick-gpt-full-stack-k27j.vercel.app/" || "http://localhost:5173",
+    credentials: true
   })
 );
 
